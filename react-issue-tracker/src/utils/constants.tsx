46937cf2e4dde2bf7c29ce25922192db503714b5
Mostafa Reda <31967263+hrr2000@users.noreply.github.com>
2023-06-16 02:06:45 +0300

    init

--- react-issue-tracker/src/utils/constants.tsx
+++ react-issue-tracker/src/utils/constants.tsx
@@ -2,7 +2,7 @@ export const GITLAB = {
   HOST: 'https://code.bld.ai',
   PROJECT_ID: 338,
   ISSUES_PER_PAGE: 100,
-  TOKEN: 'glpat-2i1WQSGQSZHFkJxwyAKJ' // PUT YOUR TOKEN
+  TOKEN: '' // PUT YOUR TOKEN
 };
 
 export const MARKDOWN = {
